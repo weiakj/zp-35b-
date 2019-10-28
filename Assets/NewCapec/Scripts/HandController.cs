00100db09fdde2cb4afcde8551bdb0f753086bad
caodengpan <1051336507@qq.com>
2019-10-28 11:29:04 +0800

    xx

---
+++ Assets/NewCapec/Scripts/HandController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+using VRTK;
+
+
+
+public class HandController : MonoBehaviour {
+
+    public Transform camTr;
+    public VRTK_Pointer pointer;
+    public GameObject pointGo;
+    public PostProcessVolume postProcessVolume;
+    private FloatParameter foucesDistance;
+	// Use this for initialization
+	void Start () {
+        pointer.DestinationMarkerEnter += OnDestinationMarkerEnter;
+        pointer.DestinationMarkerExit += OnDestinationMarkerExit;
+        pointer.DestinationMarkerHover += OnDestinationMarkerHover;
+        pointer.SelectionButtonPressed += OnSelectionButtonPressed;
+        foucesDistance = postProcessVolume.profile.GetSetting<DepthOfField>().focusDistance;
+        foucesDistance.value = 3f;
+    }
+
+    private void OnSelectionButtonPressed(object sender, ControllerInteractionEventArgs e)
+    {
+        if (pointGo != null)
+        {
+            TKPointerClick tK = pointGo.GetComponent<TKPointerClick>();
+            if (tK != null)
+            {
+                tK.OnTKPointerClick();
+            }
+        }
+    }
+
+    private void OnDestinationMarkerHover(object sender, DestinationMarkerEventArgs e)
+    {
+
+        TKPointerEnter tK = e.target.GetComponent<TKPointerEnter>();
+        if (tK != null)
+        {
+            foucesDistance.value = Mathf.Clamp(Vector3.Distance(camTr.position, e.raycastHit.point)+0.5f, 0, 3f);
+            tK.OnTKPointerEnter(e);
+        }
+    }
+
+    private void OnDestinationMarkerExit(object sender, DestinationMarkerEventArgs e)
+    {
+        pointGo = null;
+        foucesDistance.value = 3f;
+        TKPointerExit tK = e.target.GetComponent<TKPointerExit>();
+        if (tK != null)
+        {
+            tK.OnTKPointerExit(e);
+        }
+    }
+
+    private void OnDestinationMarkerEnter(object sender, DestinationMarkerEventArgs e)
+    {
+
+        pointGo = e.target.gameObject;
+        TKPointerEnter tK= e.target.GetComponent<TKPointerEnter>();
+        if (tK != null)
+        {
+            foucesDistance.value = Mathf.Clamp(Vector3.Distance(camTr.position, e.raycastHit.point)+0.5f, 0, 2.8f);
+        }
+    }
+
+    
+}
+public interface TKPointerEnter
+{
+     void OnTKPointerEnter(DestinationMarkerEventArgs e);
+}
+public interface TKPointerExit
+{
+    void OnTKPointerExit(DestinationMarkerEventArgs e);
+}
+public interface TKPointerClick
+{
+    void OnTKPointerClick();
+}
---
+++ Assets/VRTK/Grab.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class Grab : MonoBehaviour {
+
+    VRTK_InteractGrab interactGrab;
+    VRTK_Pointer vRTK_Pointer;
+    // Use this for initialization
+    void Start () {
+        interactGrab = GetComponent<VRTK_InteractGrab>();
+        vRTK_Pointer = GetComponent<VRTK_Pointer>();
+        interactGrab.GrabButtonPressed += GrabButtonReleased;
+        interactGrab.ControllerStartGrabInteractableObject += ControllerStartGrabInteractableObject;
+    }
+    private void OnDestinationMarkerHover(object sender, DestinationMarkerEventArgs e)
+    {
+        Debug.Log("MarkerHover");
+    }
+    private void ControllerStartGrabInteractableObject(object sender, ObjectInteractEventArgs e)
+    {
+        Debug.Log("GrabThings");
+    }
+    private void GrabButtonReleased(object o, ControllerInteractionEventArgs e)
+    {
+        Debug.Log("GrabPress");
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
